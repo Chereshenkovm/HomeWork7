f4d10f14a0886f2a1d3bb6f895be06cbe2876cbd
Chereshenkovm <chereshenkovm@gmail.com>
2021-11-16 14:51:07 +0300

    difficulty

---
+++ Assets/Scripts/ClassHolder.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game
+{
+    public enum BotDifficulty
+    {
+        EASY = 1,
+        MEDIUM = 2,
+        HARD = 3
+    }
+}
--- Assets/Scripts/Game/ZombieComponent.cs
+++ Assets/Scripts/Game/ZombieComponent.cs
@@ -8,6 +8,8 @@ namespace Game
 {
     public class ZombieComponent : MonoBehaviour
     {
+        [Header("ZombieAIDifficulty")] [SerializeField]
+        private BotDifficulty _botDifficulty = BotDifficulty.HARD;
         [Header("Rock Prefab")]
         [SerializeField] private GameObject _rockPrefab;
         
@@ -16,15 +18,13 @@ namespace Game
 
         [SerializeField] private Rigidbody Rigidbody;
 
-        [SerializeField] private float ZombieSpeed = 1f;
+        [SerializeField] private float ZombieSpeed = 3f;
         
         [Header("ZombieLogic")]
         
         [SerializeField] private GameObject _aliveView;
 
         [SerializeField] private GameObject _diedView;
-
-        [SerializeField] private float _speed = 5f;
 
         [SerializeField] private Rigidbody _rigidbody;
 
@@ -40,6 +40,18 @@ namespace Game
         private void Awake()
         {
             _initPosition = transform.position;
+            switch (_botDifficulty)
+            {
+                case BotDifficulty.EASY:
+                    ZombieSpeed = ZombieSpeed;
+                    break;
+                case BotDifficulty.MEDIUM:
+                    ZombieSpeed = ZombieSpeed * 1.5f;
+                    break;
+                case BotDifficulty.HARD:
+                    ZombieSpeed = ZombieSpeed * 2;
+                    break;
+            }
         }
 
         private void OnEnable()
@@ -57,7 +69,7 @@ namespace Game
                 var (moveDirection, distance, viewDirection,hasFound) = ZombieInput.CurrentInput();
                 var (moveRockDirection, rockDistance, rockTrans,isThereRocks) = ZombieInput.GetRock();
 
-                switch (getActionName(distance, rockDistance, hasFound, isThereRocks))
+                switch (getActionName(distance, rockDistance, hasFound, isThereRocks, _botDifficulty))
                 {
                     case "player":
                         Rigidbody.velocity = moveDirection.normalized * ZombieSpeed;
@@ -89,16 +101,16 @@ namespace Game
 
         public bool IsAlive => _aliveView.activeInHierarchy;
 
-        private string getActionName(Vector3 playerDist, float rockDist, bool foundPlayer, bool rocksExist)
+        private string getActionName(Vector3 playerDist, float rockDist, bool foundPlayer, bool rocksExist, BotDifficulty bDiff)
         {
             List<int> pointsAc = new List<int> {0, 0, 0, 0, 0};
 
             pointsAc[0] = (playerDist.magnitude <= 10 ? 50 : 0) + (!rocksExist ? 100 : 0) + (hasRock ? 100: 0);
             pointsAc[1] = (playerDist.magnitude > 10 ? 150 : 0) + (rockDist <= 10 ? 50 : 0) + (hasRock ? -1000 : 0) +
-                          (!rocksExist ? -1000 : 0);
-            pointsAc[2] = (rockDist < 1f ? 1000 : 0) + (hasRock ? -1000 : 0);
-            pointsAc[3] = (playerDist.magnitude < 5 ? 200 : 0) + (hasRock ? 100 : -1000);
-            pointsAc[4] = (playerDist.magnitude <= 1f ? 1001: 0);
+                          (!rocksExist ? -1000 : 0) + (bDiff == BotDifficulty.HARD ? 0 : -10000);
+            pointsAc[2] = (rockDist < 1f ? 1000 : 0) + (hasRock ? -1000 : 0) + (bDiff == BotDifficulty.HARD ? 0 : -10000);
+            pointsAc[3] = (playerDist.magnitude < 5 ? 200 : 0) + (hasRock ? 100 : -1000) + (bDiff == BotDifficulty.HARD ? 0 : -10000);
+            pointsAc[4] = (playerDist.magnitude <= 1f ? 1001 : 0) + (bDiff == BotDifficulty.EASY ? -10000 : 0);
 
             switch (pointsAc.IndexOf(pointsAc.Max()))
             {
